64f213cf43d597b23b082cfc99ae9d9c9fe7ee71
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2023-11-18 01:38:41 +0700

    Feat: config liblary

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { NextUIProvider } from '@nextui-org/react';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+      <NextNProgress color="#FACC14" height={2} showOnShallow={true} />
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
